219ba2e9bdfcd5b2979d907f973e722e2d4f0c54
BarneyAVG <barnabas.agbekorode@ontariotechu.net>
2024-11-05 16:54:16 -0500

    ccc

--- my-portfolio/app.js
+++ my-portfolio/app.js
@@ -1,33 +1,46 @@
+
+
+module.exports = app;
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Import express-ejs-layouts
+var ejsLayouts = require('express-ejs-layouts');
+
+var app = express();
+app.use(ejsLayouts);
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
 
-// view engine setup
+// Serve static files
+app.use(express.static('public'));
+
+// Set up the view engine and views directory
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// Route handlers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -36,6 +49,12 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// Set up the server to listen on a port
+var port = process.env.PORT || 3000; // Default to port 3000 or use the PORT environment variable
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
 });
 
 module.exports = app;
--- my-portfolio/routes/index.js
+++ my-portfolio/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('home', { title: 'Home' });
 });
@@ -10,12 +11,20 @@ router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About Me' });
 });
 
+router.get('/video', function(req, res) {
+  res.render('video', { title: 'Video' }); // This renders video.ejs
+});
+
 router.get('/projects', function(req, res, next) {
   res.render('projects', { title: 'Projects' });
 });
 
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact Me' });
+});
+
+router.get('/thanks', function(req, res, next) {
+  res.render('thanks', { title: 'Thank You' });
 });
 
 module.exports = router;
