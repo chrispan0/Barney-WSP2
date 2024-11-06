6ecd4237cb7c07e2f67e1bb6cd129fc6dc2de4a9
Chris Panetta <cv.panetta@gmail.com>
2024-11-05 19:14:04 -0500

    expose port 3000

--- mkdocker.bat
+++ mkdocker.bat
@@ -4,5 +4,5 @@ docker stop barney-wsp2
 docker rm barney-wsp2
 cd C:\Users\Administrator\Documents\Barney\Barney-WSP2
 docker build -t barney-wsp2 .
-docker run -d -p 3333:4000 --name barney-wsp2 barney-wsp2
+docker run -d -p 3333:3000 --name barney-wsp2 barney-wsp2
 pause
