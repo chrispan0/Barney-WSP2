d1aabc531199996b093faed022395a31109e0a5b
Chris Panetta <cv.panetta@gmail.com>
2024-11-05 19:13:43 -0500

    changed stuff

--- app.js
+++ app.js
@@ -1,36 +1,60 @@
-let createError = require("http-errors");
-let express = require("express");
-let path = require("path");
-let cookieParser = require("cookie-parser");
-let logger = require("morgan");
 
-let indexRouter = require("./routes/index");
-let usersRouter = require("./routes/users");
 
-let app = express();
+module.exports = app;
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+// Import express-ejs-layouts
+var ejsLayouts = require('express-ejs-layouts');
 
-app.use(logger("dev"));
+var app = express();
+app.use(ejsLayouts);
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+
+// Serve static files
+app.use(express.static('public'));
+
+// Set up the view engine and views directory
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+
+
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// Route handlers
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-app.use(function (req, res, next) {
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
-app.use(function (err, req, res, next) {
+// Error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
+});
+
+// Set up the server to listen on a port
+var port = process.env.PORT || 3000; // Default to port 3000 or use the PORT environment variable
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
 });
 
 module.exports = app;
