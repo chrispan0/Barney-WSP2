dcc1f6254c5aeb15a48c6867b2e50efb4aa999c9
Chris Panetta <cv.panetta@gmail.com>
2024-11-05 19:10:58 -0500

    Update mkdocker.bat

--- mkdocker.bat
+++ mkdocker.bat
@@ -1,8 +1,8 @@
 git fetch
 git pull
-docker stop milad-wsp2
-docker rm milad-wsp2
-cd C:\Users\Administrator\Documents\Milad\Milad-WSP2
-docker build -t milad-wsp2 .
-docker run -d -p 2222:4000 --name milad-wsp2 milad-wsp2
+docker stop barney-wsp2
+docker rm barney-wsp2
+cd C:\Users\Administrator\Documents\Barney\Barney-WSP2
+docker build -t barney-wsp2 .
+docker run -d -p 3333:4000 --name barney-wsp2 barney-wsp2
 pause
